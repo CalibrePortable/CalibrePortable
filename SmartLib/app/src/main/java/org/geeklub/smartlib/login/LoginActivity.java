882708ea385f36e43d6f992c6e469f97c9eab019
Vass <vass@lydiabox.com>
2014-10-26 16:34:04 +0800

    增加gitignore

--- SmartLib/app/src/main/java/org/geeklub/smartlib/login/LoginActivity.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/login/LoginActivity.java
@@ -1,10 +1,18 @@
 package org.geeklub.smartlib.login;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Vass on 2014/10/26.
  */
 public class LoginActivity extends Activity{
 
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+
+
+
+  }
 }
