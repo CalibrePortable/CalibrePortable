be074831f46c9a3b8bd7671f67f82857232dde53
Vass <vass@lydiabox.com>
2014-11-04 21:23:47 +0800

    定义好了API Service

--- SmartLib/app/src/main/java/org/geeklub/smartlib/beans/Book.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/beans/Book.java
@@ -36,7 +36,75 @@ public class Book {
    */
   private String favour;
 
-  private class Response {
+  public String getBook_name() {
+    return book_name;
+  }
+
+  public void setBook_name(String book_name) {
+    this.book_name = book_name;
+  }
+
+  public String getBook_author() {
+    return book_author;
+  }
+
+  public void setBook_author(String book_author) {
+    this.book_author = book_author;
+  }
+
+  public String getBook_type() {
+    return book_type;
+  }
+
+  public void setBook_type(String book_type) {
+    this.book_type = book_type;
+  }
+
+  public String getBook_info() {
+    return book_info;
+  }
+
+  public void setBook_info(String book_info) {
+    this.book_info = book_info;
+  }
+
+  public String getBook_price() {
+    return book_price;
+  }
+
+  public void setBook_price(String book_price) {
+    this.book_price = book_price;
+  }
+
+  public String getBook_status() {
+    return book_status;
+  }
+
+  public void setBook_status(String book_status) {
+    this.book_status = book_status;
+  }
+
+  public String getFavour() {
+    return favour;
+  }
+
+  public void setFavour(String favour) {
+    this.favour = favour;
+  }
+
+  @Override public String toString() {
+    return "Book{" +
+        "book_name='" + book_name + '\'' +
+        ", book_author='" + book_author + '\'' +
+        ", book_type='" + book_type + '\'' +
+        ", book_info='" + book_info + '\'' +
+        ", book_price='" + book_price + '\'' +
+        ", book_status='" + book_status + '\'' +
+        ", favour='" + favour + '\'' +
+        '}';
+  }
+
+  public static class Response {
     /**
      * 书本列表
      */
@@ -45,5 +113,28 @@ public class Book {
      * 状态
      */
     private int status;
+
+    public List<Book> getApps() {
+      return apps;
+    }
+
+    public void setApps(List<Book> apps) {
+      this.apps = apps;
+    }
+
+    public int getStatus() {
+      return status;
+    }
+
+    public void setStatus(int status) {
+      this.status = status;
+    }
+
+    @Override public String toString() {
+      return "Response{" +
+          "apps=" + apps +
+          ", status=" + status +
+          '}';
+    }
   }
 }
--- SmartLib/app/src/main/java/org/geeklub/smartlib/services/NormaluserService.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/services/NormaluserService.java
@@ -1,15 +1,31 @@
 package org.geeklub.smartlib.services;
 
-import org.geeklub.smartlib.beans.WebAppsReponse;
+import org.geeklub.smartlib.beans.Book;
 import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Path;
 
 /**
  * Created by Vass on 2014/11/3.
  */
 public interface NormalUserService {
-  @GET("/rank/0/{page}") void getResult(@Path("page") String page,Callback<WebAppsReponse> reponseCallback);
+  @FormUrlEncoded @POST("/normalUser/login") void login(@Field("userId") int userId,
+      @Field("password") String password, Callback<Integer> callback);
 
+  @FormUrlEncoded @POST("/normalUser/register") void register(@Field("userId") int userId,
+      @Field("password") String password, @Field("rePassword") String rePassword,
+      Callback<Integer> callback);
 
+  @FormUrlEncoded @POST("/normalUser/borrow/:bookId/:userId/:password") void borrow(
+      @Field("bookId") int bookId, @Field("userId") int userId, @Field("password") String password,
+      Callback<Integer> callback);
+
+  @GET("/search/{type}/{page}/{keyword}") void search(@Path("type") int type,
+      @Path("page") int page, @Path("keyword") String keyword, Callback<Book.Response> callback);
+
+  @GET("normalUser/return/:userId/:password") void haveBorrowed(@Path("userId") int userId,
+      @Path("password") String password, Callback<Book.Response> callback);
 }
