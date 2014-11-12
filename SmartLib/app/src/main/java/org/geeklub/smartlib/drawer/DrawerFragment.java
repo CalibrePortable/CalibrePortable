ae1fae4a06d3d0c7f794bf78f8de155a26cf22bc
Vass <vass@lydiabox.com>
2014-11-13 00:01:32 +0800

    设置clickable，修复Drawer header 透传问题

--- SmartLib/app/src/main/java/org/geeklub/smartlib/drawer/DrawerFragment.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/drawer/DrawerFragment.java
@@ -27,7 +27,6 @@ public class DrawerFragment extends BaseFragment {
 
   @InjectView(R.id.listview) ListView mDrawerList;
 
-  @InjectView(R.id.drawer_header) ImageView mDrawerHeader;
 
   public static Fragment newInstance() {
     Fragment drawerFragment = new DrawerFragment();
@@ -50,7 +49,6 @@ public class DrawerFragment extends BaseFragment {
     mDrawerList.setItemChecked(0, true);
     mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-    mDrawerHeader.setClickable(true);
 
     return view;
   }
