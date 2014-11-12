31cbe2c607f4a09ea18dd7aeee507f3233db2a33
Vass <vass@lydiabox.com>
2014-11-12 23:27:16 +0800

    增加设置界面

--- SmartLib/app/src/main/java/org/geeklub/smartlib/SettingsActivity.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/SettingsActivity.java
@@ -13,11 +13,16 @@ public class SettingsActivity extends BaseActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    //getFragmentManager().beginTransaction().replace(R.id.content_frame, ).commit();
+    setTitle(getString(R.string.action_settings));
+
+    getFragmentManager().beginTransaction()
+        .replace(R.id.content_frame, new SettingsFragment())
+        .commit();
   }
 
   @Override protected void initActionBar() {
     ActionBar actionBar = getSupportActionBar();
+    actionBar.setDisplayShowTitleEnabled(false);
     actionBar.setDisplayHomeAsUpEnabled(true);
     actionBar.setDisplayShowHomeEnabled(true);
   }
--- SmartLib/app/src/main/java/org/geeklub/smartlib/SettingsFragment.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/SettingsFragment.java
@@ -1,10 +1,44 @@
 package org.geeklub.smartlib;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 
 /**
  * Created by Vass on 2014/11/12.
  */
-public class SettingsFragment extends BaseFragment {
+public class SettingsFragment extends PreferenceFragment
+    implements Preference.OnPreferenceChangeListener {
+
+  private Preference versionPreference;
+
+  @Override public boolean onPreferenceChange(Preference preference, Object newValue) {
+    return false;
+  }
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    addPreferencesFromResource(R.xml.settings);
+
+    versionPreference = findPreference(getString(R.string.settings_key_version));
+
+    try {
+      setVersion();
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
 
 
+  }
+
+  private void setVersion() throws PackageManager.NameNotFoundException {
+
+    PackageInfo packageInfo = getActivity().getPackageManager()
+        .getPackageInfo(getActivity().getPackageName(), PackageManager.GET_CONFIGURATIONS);
+
+    versionPreference.setTitle(packageInfo.versionName);
+  }
 }
