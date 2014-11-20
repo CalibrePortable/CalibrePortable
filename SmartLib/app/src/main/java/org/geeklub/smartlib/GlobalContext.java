279dbc5f6ade2199b78ee460669d132bf312678f
Vass <vass@lydiabox.com>
2014-11-20 20:11:31 +0800

    完成了用户反馈功能

--- SmartLib/app/src/main/java/org/geeklub/smartlib/GlobalContext.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/GlobalContext.java
@@ -2,6 +2,7 @@ package org.geeklub.smartlib;
 
 import android.app.Application;
 import android.content.Context;
+import com.avos.avoscloud.AVOSCloud;
 
 /**
  * Created by Vass on 2014/10/7.
@@ -18,5 +19,7 @@ public class GlobalContext extends Application {
     super.onCreate();
 
     sContext = getApplicationContext();
+    AVOSCloud.initialize(sContext, "skr3lpa65qd2maqwvl6b00k9ra1ecvc6usloso5in3kw8e9s",
+        "2t8luwbujuvpfwv4xjswips2y4fqim56yeptjtm5lclzv6fb");
   }
 }
