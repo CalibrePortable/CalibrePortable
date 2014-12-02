b9266d38c9806aa93d46c0fd40f069963b910d54
Vass <vass@lydiabox.com>
2014-12-02 22:15:30 +0800

    修复点赞bug

--- SmartLib/app/src/main/java/org/geeklub/smartlib/module/library/LibraryFragment.java
+++ SmartLib/app/src/main/java/org/geeklub/smartlib/module/library/LibraryFragment.java
@@ -11,8 +11,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import butterknife.InjectView;
+
 import java.util.List;
+
 import org.geeklub.smartlib.GlobalContext;
 import org.geeklub.smartlib.R;
 import org.geeklub.smartlib.beans.ServerResponse;
@@ -24,6 +27,7 @@ import org.geeklub.smartlib.module.detail.BookDetailActivity;
 import org.geeklub.smartlib.utils.LogUtil;
 import org.geeklub.smartlib.utils.SmartLibraryUser;
 import org.geeklub.smartlib.utils.ToastUtil;
+
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -33,136 +37,150 @@ import retrofit.client.Response;
  */
 public class LibraryFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener {
 
-  @InjectView(R.id.swipe_layout) SwipeRefreshLayout mRefreshLayout;
-  @InjectView(R.id.recycle_view) RecyclerView mRecyclerView;
+    @InjectView(R.id.swipe_layout)
+    SwipeRefreshLayout mRefreshLayout;
+    @InjectView(R.id.recycle_view)
+    RecyclerView mRecyclerView;
 
-  private LibraryAdapter mAdapter;
-  private int mPage = 1;
+    private LibraryAdapter mAdapter;
+    private int mPage = 1;
 
-  public static Fragment newInstance() {
-    Fragment libraryFragment = new LibraryFragment();
-    return libraryFragment;
-  }
-
-  @Override public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    mAdapter = new LibraryAdapter(mActivity);
-
-    mAdapter.setOnItemClickListener(new LibraryAdapter.OnItemClickListener() {
-      @Override public void onItemClick(SummaryBook book) {
-        Intent intent = new Intent(mActivity, BookDetailActivity.class);
-        intent.putExtra(BookDetailActivity.EXTRAS_BOOK, book);
-        startActivity(intent);
-      }
-    });
-
-    mAdapter.setOnFavourClickListener(new LibraryAdapter.OnFavourClickListener() {
-      @Override public void onFavourClick(SummaryBook book) {
-        book.favour = "1";
-        book.isLike = Integer.valueOf(book.isLike) + 1 + "";
-        sendDianZanMsg(book);
-      }
-    });
-  }
-
-  private void sendDianZanMsg(SummaryBook book) {
-    SmartLibraryUser user = SmartLibraryUser.getCurrentUser();
-    NormalUserService service = GlobalContext.getApiDispencer().getRestApi(NormalUserService.class);
-
-    service.likePlusOne(book.book_kind, user.getUserId(), user.getPassWord(),
-        new Callback<ServerResponse>() {
-          @Override public void success(ServerResponse serverResponse, Response response) {
-            LogUtil.i("点赞成功 ...");
-          }
-
-          @Override public void failure(RetrofitError error) {
-            LogUtil.i("点赞失败 ===>>>" + error.getMessage());
-          }
-        });
-  }
-
-  @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-
-    View view = super.onCreateView(inflater, container, savedInstanceState);
-
-    mRefreshLayout.setOnRefreshListener(this);
-    mRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
-        android.R.color.holo_green_light, android.R.color.holo_orange_light,
-        android.R.color.holo_red_light);
-
-    mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
-    mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-    mRecyclerView.setHasFixedSize(true);
-    mRecyclerView.setAdapter(mAdapter);
-
-    mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
-      @Override public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-        super.onScrollStateChanged(recyclerView, newState);
-
-        switch (newState) {
-          case RecyclerView.SCROLL_STATE_IDLE:
-            loadNextPage();
-            break;
-
-          default:
-            break;
-        }
-      }
-    });
-
-    loadFirstPage();
-
-    return view;
-  }
-
-  private void loadNextPage() {
-    loadData(mPage);
-  }
-
-  private void loadFirstPage() {
-    mPage = 1;
-    loadData(mPage);
-  }
-
-  private void loadData(int page) {
-    SmartLibraryUser user = SmartLibraryUser.getCurrentUser();
-    NormalUserService service = GlobalContext.getApiDispencer().getRestApi(NormalUserService.class);
-
-    final boolean isRefreshFromTop = (page == 1);
-    if (!mRefreshLayout.isRefreshing() && isRefreshFromTop) {
-      mRefreshLayout.setRefreshing(true);
+    public static Fragment newInstance() {
+        Fragment libraryFragment = new LibraryFragment();
+        return libraryFragment;
     }
 
-    service.search(user.getUserId(), 5, page, "all", new Callback<List<SummaryBook>>() {
-      @Override public void success(List<SummaryBook> summaryBooks, Response response) {
-        mRefreshLayout.setRefreshing(false);
-        if (isRefreshFromTop) {
-          LogUtil.i("isRefreshFromTop ===>>>" + isRefreshFromTop);
-          if (mAdapter.equals(summaryBooks)) {
-            ToastUtil.showShort("没有新的书本...");
-          } else {
-            mAdapter.replaceWith(summaryBooks);
-          }
-        } else {
-          mAdapter.addAll(summaryBooks);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAdapter = new LibraryAdapter(mActivity);
+
+        mAdapter.setOnItemClickListener(new LibraryAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(SummaryBook book) {
+                Intent intent = new Intent(mActivity, BookDetailActivity.class);
+                intent.putExtra(BookDetailActivity.EXTRAS_BOOK, book);
+                startActivity(intent);
+            }
+        });
+
+        mAdapter.setOnFavourClickListener(new LibraryAdapter.OnFavourClickListener() {
+            @Override
+            public void onFavourClick(SummaryBook book) {
+                book.favour = "1";
+                book.isLike = Integer.valueOf(book.isLike) + 1 + "";
+                sendDianZanMsg(book);
+            }
+        });
+    }
+
+    private void sendDianZanMsg(SummaryBook book) {
+        SmartLibraryUser user = SmartLibraryUser.getCurrentUser();
+        NormalUserService service = GlobalContext.getApiDispencer().getRestApi(NormalUserService.class);
+
+        service.likePlusOne(book.book_kind, user.getUserId(), user.getPassWord(),
+                new Callback<ServerResponse>() {
+                    @Override
+                    public void success(ServerResponse serverResponse, Response response) {
+                        LogUtil.i("点赞成功 ...");
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        LogUtil.i("点赞失败 ===>>>" + error.getMessage());
+                    }
+                });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        mRefreshLayout.setOnRefreshListener(this);
+        mRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                switch (newState) {
+                    case RecyclerView.SCROLL_STATE_IDLE:
+                        loadNextPage();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+
+        loadFirstPage();
+
+        return view;
+    }
+
+    private void loadNextPage() {
+        loadData(mPage);
+    }
+
+    private void loadFirstPage() {
+        mPage = 1;
+        loadData(mPage);
+    }
+
+    private void loadData(int page) {
+        SmartLibraryUser user = SmartLibraryUser.getCurrentUser();
+        NormalUserService service = GlobalContext.getApiDispencer().getRestApi(NormalUserService.class);
+
+        final boolean isRefreshFromTop = (page == 1);
+        if (!mRefreshLayout.isRefreshing() && isRefreshFromTop) {
+            mRefreshLayout.setRefreshing(true);
         }
-        mPage++;
-      }
 
-      @Override public void failure(RetrofitError error) {
-        mRefreshLayout.setRefreshing(false);
-        ToastUtil.showShort("Failed to load. Try again later...");
-      }
-    });
-  }
+        service.search(user.getUserId(), 5, page, "all", new Callback<List<SummaryBook>>() {
+            @Override
+            public void success(List<SummaryBook> summaryBooks, Response response) {
+                mRefreshLayout.setRefreshing(false);
+                if (isRefreshFromTop) {
+                    LogUtil.i("isRefreshFromTop ===>>>" + isRefreshFromTop);
+                    if (mAdapter.equals(summaryBooks)) {
+                        ToastUtil.showShort("没有新的书本...");
+                    } else {
+                        mAdapter.replaceWith(summaryBooks);
+                    }
+                } else {
+                    mAdapter.addAll(summaryBooks);
+                }
+                mPage++;
+            }
 
-  @Override protected int getLayoutResource() {
-    return R.layout.fragment_library;
-  }
+            @Override
+            public void failure(RetrofitError error) {
+                mRefreshLayout.setRefreshing(false);
+                ToastUtil.showShort("Failed to load. Try again later...");
+            }
+        });
+    }
 
-  @Override public void onRefresh() {
-    loadFirstPage();
-  }
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_library;
+    }
+
+    @Override
+    public void onRefresh() {
+        loadFirstPage();
+    }
 }
