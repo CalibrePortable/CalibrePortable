5355078366aec4b800dad958142fb703f9f6de8f
Vass <vass@lydiabox.com>
2014-12-08 10:31:32 +0800

    修改样式文件，增加一些资源文件

--- SmartLib4Admin/app/src/main/java/org/geeklub/smartlib4admin/module/detail/BookDetailActivity.java
+++ SmartLib4Admin/app/src/main/java/org/geeklub/smartlib4admin/module/detail/BookDetailActivity.java
@@ -1,13 +1,29 @@
 package org.geeklub.smartlib4admin.module.detail;
 
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.geeklub.smartlib4admin.R;
 import org.geeklub.smartlib4admin.module.base.BaseActivity;
+
+import butterknife.InjectView;
 
 /**
  * Created by Vass on 2014/12/2.
  */
 public class BookDetailActivity extends BaseActivity {
+
+
+
+
+
     @Override
     protected int getLayoutResource() {
-        return 0;
+        return R.layout.activity_detail_book;
     }
+
+
+
 }
