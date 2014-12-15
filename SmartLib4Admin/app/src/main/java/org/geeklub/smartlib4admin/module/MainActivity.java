2f44b9d1da17eef67b5b5e575159eefd62c9405b
Vass <vass@lydiabox.com>
2014-12-15 10:40:03 +0800

    修复扫描二维码bug

--- SmartLib4Admin/app/src/main/java/org/geeklub/smartlib4admin/module/MainActivity.java
+++ SmartLib4Admin/app/src/main/java/org/geeklub/smartlib4admin/module/MainActivity.java
@@ -217,7 +217,7 @@ public class MainActivity extends BaseActivity
 
     private void NewQRCodeInstance() {
         IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
         integrator.setPrompt("扫描书本的二维码");
         integrator.setResultDisplayDuration(0);
         integrator.setCameraId(0);  // Use a specific camera of the device
